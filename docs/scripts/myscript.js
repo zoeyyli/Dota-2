1cd51a6f44eaaba457c5104e6b13a76e09574744
Ziyi Li <zl3360@columbia.edu>
2023-12-14 13:18:27 -0500

    added one more d3 graph

--- docs/scripts/myscript.js
+++ docs/scripts/myscript.js
@@ -1,8 +1,8 @@
 
 const w = 700;
 const h = 500;
-const margin = {top: 25, right: 50, bottom: 25,
-  left: 50};
+const margin = {top: 25, right: 60, bottom: 25,
+  left: 60};
 const innerWidth = w - margin.left - margin.right;
 const innerHeight = h - margin.top - margin.bottom;
 
@@ -1098,3 +1098,679 @@ d3.select('input[id="10"]')
         .text("Oh this is a bad hero! Choose another one!");
     });
 
+
+const svg_3 = d3.select("div#plot_3")
+.append("svg")
+.attr("width", w)
+.attr("height", h);
+
+const bar_1 = [6.055000, 5.916667, 5.083333, 17.722222]
+d3.select('input[id="Q"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_1.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_2)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_1);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_2 = [4.723548, 8.064516, 6.451613, 16.354839]
+d3.select('input[id="R"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_2.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_2)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_2);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_3 = [2.560541, 7.432432, 7.648649, 9.540541]
+d3.select('input[id="S"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_3.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_3)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_3);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_4 = [3.880000, 6.212121, 7.030303, 13.060606]
+d3.select('input[id="T"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_4.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_4)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_4);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_5 = [2.739412, 5.970588, 7.029412, 12.411765]
+d3.select('input[id="U"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_5.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_5)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_5);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_6 = [3.321111, 8.166667, 7.750000, 15.472222]
+d3.select('input[id="V"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_6.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_6)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_6);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_7 = [5.755625, 10.156250, 5.312500, 13.468750]
+d3.select('input[id="W"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_7.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_7)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_7);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_8 = [3.302500, 5.750000, 6.694444, 11.972222]
+d3.select('input[id="X"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_8.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_8)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_8);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_9 = [2.837097, 6.064516, 6.612903, 9.645161]
+d3.select('input[id="Y"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_9.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_9)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_9);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+});
+
+const bar_10 = [5.070303, 10.939394, 5.727273, 12.666667]
+d3.select('input[id="Z"]')
+  .on("click", function(event) {
+    svg_3.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "lightgray");
+
+     // Define scales and axes for the horizontal bar chart
+  const yScale = d3.scaleBand()
+    .domain(d3.range(bar_10.length))
+    .range([0, innerHeight])
+    .paddingInner(0.1);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(bar_10)])
+    .range([0, innerWidth]);
+
+  const yAxis = d3.axisLeft().scale(yScale);
+  const xAxis = d3.axisBottom().scale(xScale);
+
+  const bars = svg_3.append("g")
+    .attr("id", "plot")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .selectAll("rect")
+    .data(bar_10);
+
+  bars.enter().append("rect")
+    .attr("y", (d, i) => yScale(i))
+    .attr("x", 0)  // Horizontal position starts from 0
+    .attr("height", yScale.bandwidth())
+    .attr("width", d => xScale(d))
+    .attr("fill", d => "blue");
+
+  svg_3.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .call(yAxis);
+
+  svg_3.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${margin.left}, ${h - margin.bottom})`)
+    .call(xAxis);
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 90)
+    .text("kda");
+
+  svg_3.append("text")
+    .attr("x", 7)
+    .attr("y", 210)
+    .text("kills");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 330)
+    .text("death");
+
+  svg_3.append("text")
+    .attr("x", 5)
+    .attr("y", 440)
+    .text("assist");
+
+});
